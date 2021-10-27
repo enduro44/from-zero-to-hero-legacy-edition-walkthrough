1b52602d3f7efc96853095a2a58bd5bb2fb24199
jskesters <juris.skesters@gmail.com>
2021-10-27 20:07:29 +0300

    Unit tests

---
+++ tests/OrderTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests;
+
+class OrderTest extends \Codeception\Test\Unit
+{
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
