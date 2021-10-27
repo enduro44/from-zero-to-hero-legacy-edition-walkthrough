4c50b3cd1b4caa7ab94fdedf5875a3cdef973031
jskesters <juris.skesters@gmail.com>
2021-10-27 18:53:45 +0300

    Add Codeception testing framework
    Testing is essential for every project. We will use Codeception with Unit tests for this project

---
+++ tests/unit/OrderCest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests;
+use Tests\UnitTester;
+class OrderCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
