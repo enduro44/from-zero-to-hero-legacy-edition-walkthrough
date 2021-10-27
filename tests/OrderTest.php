b5521b1f16cc651231e58458bd310c988b60d308
jskesters <juris.skesters@gmail.com>
2021-10-27 22:09:42 +0300

    Add banklink test

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Tests;
+
+
+use App\Order;
 
 class OrderTest extends \Codeception\Test\Unit
 {
@@ -13,8 +17,14 @@ class OrderTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testSomeFeature()
+    public function testPaymentByBanklink()
     {
-
+        $order = new Order();
+        $securityCode = '12';
+        $order->pay('banklink', $securityCode);
+        $this->expectOutputRegex('/Processing banklink payment type/');
+        $this->expectOutputRegex(sprintf('/Verifying security code: %s/', $securityCode));
+        $this->assertEquals('paid', $order->status);
     }
+
 }
