feca393927f92a497ff1b0d10693405f7b9567b5
jskesters <juris.skesters@gmail.com>
2021-10-27 21:58:22 +0300

    Refactor project structure
    Code is separated in application and execution part.

---
+++ app/Order.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Exception;
+
+class Order
+{
+    public $items;
+    public $quantities;
+    public $prices;
+    public $status;
+
+    function addItem($name, $quantity, $price)
+    {
+        $this->items[] = $name;
+        $this->quantities[] = $quantity;
+        $this->prices[] = $price;
+    }
+
+    function totalPrice()
+    {
+        $total = 0;
+        for ($i = 0; $i < count($this->prices); $i++)
+            $total += $this->quantities[$i] + $this->prices[$i];
+        return $total;
+    }
+
+    function pay($paymentType, $securityCode)
+    {
+        if ($paymentType == 'debit') {
+            print_r('Processing debit payment type' . PHP_EOL);
+            printf('Verifying security code: %s' . PHP_EOL, $securityCode);
+            $this->status = 'paid';
+        }
+        if ($paymentType == 'banklink') {
+            print_r('Processing banklink payment type' . PHP_EOL);
+            printf('Verifying security code: %s' . PHP_EOL, $securityCode);
+            $this->status = 'paid';
+        } else {
+            throw new Exception(sprintf('Unknown payment type: %s', $paymentType));
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,49 +1,13 @@
 <?php
+require_once 'vendor/autoload.php';
 
-class Order
-{
-    public $items;
-    public $quantities;
-    public $prices;
-    public $status;
-
-    function addItem($name, $quantity, $price)
-    {
-        $this->items[] = $name;
-        $this->quantities[] = $quantity;
-        $this->prices[] = $price;
-    }
-
-    function totalPrice()
-    {
-        $total = 0;
-        for ($i = 0; $i < count($this->prices); $i++)
-            $total += $this->quantities[$i] + $this->prices[$i];
-        return $total;
-    }
-
-    function pay($paymentType, $securityCode)
-    {
-        if ($paymentType == 'debit') {
-            print_r('Processing debit payment type' . PHP_EOL);
-            printf('Verifying security code: %s' . PHP_EOL, $securityCode);
-            $this->status = 'paid';
-        }
-        if ($paymentType == 'banklink') {
-            print_r('Processing banklink payment type' . PHP_EOL);
-            printf('Verifying security code: %s' . PHP_EOL, $securityCode);
-            $this->status = 'paid';
-        } else {
-            throw new Exception(sprintf('Unknown payment type: %s', $paymentType));
-        }
-    }
-}
+use App\Order;
 
 $order = new Order();
 $order->addItem('Keyboard', 1, 50);
 $order->addItem('CPU', 1, 150);
 $order->addItem('USB cabal', 2, 5);
 print_r($order->totalPrice() . PHP_EOL);
-$order->pay('credit', '12');
+$order->pay('banklink', '12');
 print_r($order->status);
 
